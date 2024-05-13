019ffbe798a35cf69d33fa64a681c1cc358c3a26
Surajsinh <surajsinh.solanki@solutionanalysts.com>
2024-05-13 14:57:31 +0530

    [fix] change env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,17 @@
-// export const environment = {
-//   production: false,
-//   API_URL: 'http://13.235.28.60:3001/',
-//   RAZOR_PAY_KEY: '',
-//   CASH_FREE_KEY: '',
-//   RETURN_URL : 'http://13.235.28.60:3001/',
-//   MEDIA_URL : 'https://dev-ms.taskopad.com/'
-// };
-
 export const environment = {
   production: false,
-  API_URL: 'http://127.0.0.1:3001/',
+  API_URL: 'http://13.235.28.60:3001/',
   RAZOR_PAY_KEY: '',
   CASH_FREE_KEY: '',
-  RETURN_URL : 'http://127.0.0.1:3001/',
+  RETURN_URL : 'http://13.235.28.60:3001/',
   MEDIA_URL : 'https://dev-ms.taskopad.com/'
 };
+
+// export const environment = {
+//   production: false,
+//   API_URL: 'http://127.0.0.1:3001/',
+//   RAZOR_PAY_KEY: '',
+//   CASH_FREE_KEY: '',
+//   RETURN_URL : 'http://127.0.0.1:3001/',
+//   MEDIA_URL : 'https://dev-ms.taskopad.com/'
+// };
