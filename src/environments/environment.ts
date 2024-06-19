a87bf8197aef1cec47cfcb7e2b0ca349c3f01078
Surajsinh.solanki <Surajsinh.solanki@solutionanalysts.com>
2024-06-19 11:38:06 +0530

    [fix] change env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  API_URL: 'https://dev-api.aarambhfashion.com',
+  API_URL: 'https://dev-api.aarambhfashion.com/',
   RAZOR_PAY_KEY: '',
   CASH_FREE_KEY: '',
-  RETURN_URL: 'https://dev-api.aarambhfashion.com',
+  RETURN_URL: 'https://dev-api.aarambhfashion.com/',
   MEDIA_URL: 'https://dev-ms.taskopad.com/',
 };
 
